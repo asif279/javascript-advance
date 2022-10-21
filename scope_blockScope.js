f7785420baade6aa5672d44914a3de0248379108
asif279 <asifofficial8130@gmail.com>
2022-10-21 23:38:01 +0600

    add

--- scope_blockScope.js
+++ scope_blockScope.js
@@ -1,12 +1,23 @@
-let bonus =20;
+// let bonus =20;
 
-function sum(n1,n2){
+// function sum(n1,n2){
 
-    let result =n1+n2+bonus;
-    console.log(bonus);
-    return result;
+//     let result =n1+n2+bonus;
+//     console.log(bonus);
+//     return result;
+
+// }
+
+// const output = sum(12,13);
+// console.log(bonus);
+if( 'false' ){
+
+    console.log("I am not false")
 
 }
 
-const output = sum(12,13);
-console.log(bonus);
+else{
+
+    console.log("I am false")
+
+}
