26b73c034f00099bf91819e295feaf293142575d
asif279 <asifofficial8130@gmail.com>
2022-10-21 11:24:32 +0600

    add

--- doubleVstriplequal.js
+++ doubleVstriplequal.js
@@ -1,18 +1,18 @@
-const one = 5;
-const two = 5;
+// const one = 5;
+// const two = 5;
 
-if(one == two){
-    console.log("they are true");
-}
-else{
-    console.log("they are false");
-}
-const one = 5;
-const two ="5";
+// if(one == two){
+//     console.log("they are true");
+// }
+// else{
+//     console.log("they are false");
+// }
+// const one = 5;
+// const two ="5";
 
-if(one === two){
-    console.log("they are true");
-}
-else{
-    console.log("they are false");
-}
+// if(one === two){
+//     console.log("they are true");
+// }
+// else{
+//     console.log("they are false");
+// }
---
+++ map-filter.js
@@ -0,0 +1,21 @@
+const num =[3,4,5,6,7,8];
+// const output =[];
+
+// for (let i = 0; i < num.length; i++) {
+//     const element = num[i];
+//     const result = element*element;
+//     output.push(result);
+    
+// }
+// console.log(output);
+
+// function square(element){
+
+//     return element * element;
+// }
+//  const result =num.map = function(element){
+//     return element*element;
+// }
+//const result = num.map(x=> x*x);
+const result = num.filter(x=>x>5);
+console.log(result)
