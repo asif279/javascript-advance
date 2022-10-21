237d7d7c6679a1c9cb68560d86cae9a8dc6e5bc9
asif279 <asifofficial8130@gmail.com>
2022-10-21 14:29:02 +0600

    add

---
+++ objectpropertymap.js
@@ -0,0 +1,14 @@
+const student=[
+
+    {name:"asof",id:2031},
+    {name:"asof",id:2031},
+    {name:"asof",id:2031},
+    {name:"asof",id:2031}
+];
+
+const names = student.map(s=>s.name);
+const ids = student.map(s=>s.id);
+const big = student.filter(s=>s.id>20);
+
+
+
