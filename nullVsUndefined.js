fb214edb6c6b3c9c511e987f3a8f3dde6652b693
asif279 <asifofficial8130@gmail.com>
2022-10-21 10:50:50 +0600

    Null

---
+++ nullVsUndefined.js
@@ -0,0 +1,18 @@
+// // let Rizik;
+// // console.log(Rizik);
+
+
+// // function subtract(num1,num2){
+
+// //     console.log(num1 +num2);
+// //if I didnt pass parameter then it will be Undefined
+// //     return 
+// // }
+// //  const result = subtract(15,12);
+// //  console.log(result);
+// const Love = {name:"K", number:1231};
+// console.log(Love.gf);
+
+
+
+
